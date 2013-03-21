91a88e30a86ad97ee90ec2e73a432dc785904e30
stansage <stansage@ya.ru>
2013-03-21 19:29:14 +0700

    Add minimal comment to typemap.dat

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,16 +2,8 @@
 
 int main(int argc, char ** argv)
 {
-//    onvif::RemoteDiscovery * client = onvif::RemoteDiscovery::client();
-//    onvif::RemoteDiscovery * server = onvif::RemoteDiscovery::server();
-//    delete client;
-//    delete server;
-
     auto proxy = onvifxx::createProxy<onvifxx::RemoteDiscovery>();
     delete proxy;
 
     auto service = onvifxx::createService<onvifxx::RemoteDiscovery>();
     delete service;
-}
-
-
