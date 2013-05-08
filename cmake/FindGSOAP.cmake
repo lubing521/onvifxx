69f5ddeea774b5d814e1fba6cdf3c361ec4e206c
stansage <stansage@ya.ru>
2013-05-08 20:38:40 +0700

    Correct the dependencies from external packages.

--- cmake/FindGSOAP.cmake
+++ cmake/FindGSOAP.cmake
@@ -12,7 +12,7 @@
 # GSOAP_SOAPCPP2        = soapcpp2 binary
 # GSOAP_FOUND           = set to true if gsoap was found successfully
 #
-# GSOAP_LOCATION
+# GSOAP_DIR
 #   setting this enables search for gsoap libraries / headers in this location
 
 # -----------------------------------------------------
@@ -20,7 +20,7 @@
 # -----------------------------------------------------
 find_path(GSOAP_IMPORT_DIR
   NAMES wsa.h
-  PATHS ${GSOAP_LOCATION}/import ${GSOAP_LOCATION}/share/gsoap/import
+  PATHS ${GSOAP_DIR}/import ${GSOAP_DIR}/share/gsoap/import
 )
 
 # -----------------------------------------------------
@@ -28,14 +28,14 @@ find_path(GSOAP_IMPORT_DIR
 # -----------------------------------------------------
 find_library(GSOAP_LIBRARIES
 	NAMES gsoap++
-	HINTS ${GSOAP_LOCATION}/lib ${GSOAP_LOCATION}/lib64
-		  ${GSOAP_LOCATION}/lib32
+	HINTS ${GSOAP_DIR}/lib ${GSOAP_DIR}/lib64
+		  ${GSOAP_DIR}/lib32
 	DOC "The main gsoap library"
 )
 find_library(GSOAP_SSL_LIBRARIES
 	NAMES gsoapssl++
-	HINTS ${GSOAP_LOCATION}/lib ${GSOAP_LOCATION}/lib64
-		  ${GSOAP_LOCATION}/lib32
+	HINTS ${GSOAP_DIR}/lib ${GSOAP_DIR}/lib64
+		  ${GSOAP_DIR}/lib32
 	DOC "The ssl gsoap library"
 )
 
@@ -44,7 +44,7 @@ find_library(GSOAP_SSL_LIBRARIES
 # -----------------------------------------------------
 find_path(GSOAP_INCLUDE_DIR
 	NAMES stdsoap2.h
-	HINTS ${GSOAP_LOCATION} ${GSOAP_LOCATION}/include ${GSOAP_LOCATION}/include/*
+	HINTS ${GSOAP_DIR} ${GSOAP_DIR}/include ${GSOAP_DIR}/include/*
 	DOC "The gsoap include directory"
 )
 
@@ -53,24 +53,27 @@ find_path(GSOAP_INCLUDE_DIR
 # -----------------------------------------------------
 find_program(GSOAP_WSDL2H
 	NAMES wsdl2h
-	HINTS ${GSOAP_LOCATION}/bin
+	HINTS ${GSOAP_DIR}/bin
 	DOC "The gsoap bin directory"
 )
 find_program(GSOAP_SOAPCPP2
 	NAMES soapcpp2
-	HINTS ${GSOAP_LOCATION}/bin
+	HINTS ${GSOAP_DIR}/bin
 	DOC "The gsoap bin directory"
 )
+
+# -----------------------------------------------------
+# GSOAP version
+# try to determine the flagfor the 2.7.6 compatiblity, break with 2.7.13 and re-break with 2.7.16
+# ----------------------------------------------------
+
+execute_process(COMMAND ${GSOAP_SOAPCPP2}  "-v"   OUTPUT_VARIABLE GSOAP_STRING_VERSION ERROR_VARIABLE GSOAP_STRING_VERSION )
+string(REGEX MATCH "[0-9]*\\.[0-9]*\\.[0-9]*" GSOAP_VERSION ${GSOAP_STRING_VERSION})
 
 # -----------------------------------------------------
 # GSOAP_276_COMPAT_FLAGS and GSOAPVERSION
 # try to determine the flagfor the 2.7.6 compatiblity, break with 2.7.13 and re-break with 2.7.16
 # ----------------------------------------------------
-message(STATUS " - wsdlh : ${GSOAP_WSDL2H}")
-message(STATUS " - SOAPCPP2 : ${GSOAP_SOAPCPP2}")
-execute_process(COMMAND ${GSOAP_SOAPCPP2}  "-v"   OUTPUT_VARIABLE GSOAP_STRING_VERSION ERROR_VARIABLE GSOAP_STRING_VERSION )
-string(REGEX MATCH "[0-9]*\\.[0-9]*\\.[0-9]*" GSOAP_VERSION ${GSOAP_STRING_VERSION})
-message(STATUS " - GSOAP VERSION : ${GSOAP_VERSION}")
 if( "${GSOAP_VERSION}"  VERSION_LESS "2.7.6")
 	set(GSOAP_276_COMPAT_FLAGS "")
 elseif ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.14")
@@ -78,7 +81,6 @@ elseif ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.14")
 else ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.14")
 	set(GSOAP_276_COMPAT_FLAGS "-z1 -z2")
 endif ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.6")
-
 
 
 # -----------------------------------------------------
@@ -89,3 +91,7 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(gsoap DEFAULT_MSG GSOAP_LIBRARIES
 	GSOAP_INCLUDE_DIR GSOAP_WSDL2H GSOAP_SOAPCPP2)
 mark_as_advanced(GSOAP_INCLUDE_DIR GSOAP_LIBRARIES GSOAP_WSDL2H GSOAP_SOAPCPP2)
+
+if(GSOAP_FOUND AND GSOAP_FIND_REQUIRED AND GSOAP_FIND_VERSION AND ${GSOAP_VERSION} VERSION_LESS ${GSOAP_FIND_VERSION})
+	message(SEND_ERROR "Found GSOAP version ${GSOAP_VERSION} less then required ${GSOAP_FIND_VERSION}.")
+endif()
